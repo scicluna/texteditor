fed8127544d76f4f3b4f27cb7fd3dfe016d38655
scicluna <88948700+scicluna@users.noreply.github.com>
2023-04-11 16:44:50 -0400

    Massive firebase adventure. turns out not good alternative

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -2,6 +2,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const WebpackPwaManifest = require('webpack-pwa-manifest');
 const path = require('path');
 const { InjectManifest } = require('workbox-webpack-plugin');
+const { initializeApp } = require('firebase/app')
 
 module.exports = () => {
   return {
